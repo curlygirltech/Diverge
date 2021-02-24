c1dcbea004be51af3b8aec54f54efbc9f48383b3
jcameron10 <jovonnec@gmail.com>
2021-02-24 16:20:27 -0500

    added buttons

--- app.js
+++ app.js
@@ -18,31 +18,53 @@ async function getMusic(artist) {
 
 function fetchData (data) {
   const dataContainer = document.querySelector('#artist-data')
+  const artistArr = Object.entries(data)
+  console.log(artistArr)
+  let languageBtn = artistArr.map((arr) => {
+    
+  })
   const artistInfo = `
-  <h1>${data.strArtist}</h1>
+  <h1 class="">${data.strArtist}</h1>
   <img src= "${data.strArtistClearart}" alt="Artist pic" class = "Artist-pic"/>
   <h2>${data.strGenre}</h2>
   <p>${data.strWebsite}</p>
   <p>${data.strTwitter}</p>
   <p>${data.strFacebook}</p>   
   <p>${data.strBiographyEN}</p>
+  <button class="language-btn" value="ES">ES</button>
+  <button class="language-btn" value="FR">FR</button>
+  <button class="language-btn" value="CN">CN</button>
+  <button class="language-btn" value="JP">JP</button>
+  <button class="language-btn" value="IT">IT</button>
+  <button class="language-btn" value="PT">PT</button>
+  <button class="language-btn" value="DE">DE</button>
   `
   console.log(artistInfo)
   dataContainer.insertAdjacentHTML('beforeend', artistInfo)
 
-  if (`${data.strArtistClearart}` === 0) {
-    `${data.strArtistBanner}`
-  } 
+  // if (`${data.strArtistClearart}` === 0) {
+  //   `${data.strArtistBanner}`
+  // } 
 } 
 
 // 4.Append to Artist info
 // switch language Bios
-dataContainer.querySelector('#artist-data') =
-  `
-<p>${data.strBiographyES}</p>
-<p>${data.strBiographyFR}</p>
-<p>${data.strBiographyDE}</p>
+// dataContainer.querySelector('#artist-data') =
+//   `
+// <p>${data.strBiographyES}</p>
+// <p>${data.strBiographyFR}</p>
+// <p>${data.strBiographyDE}</p>
+// <p>${data.strBiographyCN}</p>
+// <p>${data.strBiographyJP}</p>
+// <p>${data.strBiographyIT}</p>
+// <p>${data.strBiographyPT}</p>
+// `
 
+
+
+
+
+// 4.Append to Artist info
 const form = document.querySelector('form')
 
 // Add event handler to form 
