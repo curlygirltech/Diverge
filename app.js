5b902dd318e6bdb568c1282e865a2b33b7241dc8
jcameron10 <jovonnec@gmail.com>
2021-02-23 20:54:59 -0500

    Added AdjacentHtml

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-console.log('hey, its working')
 // const apiAddress = 'theaudiodb.com/api/v1/json/1/'
 
 
-async function getMusic() {
-  const url = 'theaudiodb.com/api/v1/json/1/search.php?s='
+async function getMusic(artist) {
+  const url = `http://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
   try {
     let response = await axios.get(url)
-    let data = response.data
+    let data = response.data.artists
     console.log(data)
   } catch (error) {
-    console.error(err) 
+    console.error(error) 
   }
   
   }
-getMusic('beyonce')
+getMusic('drake')
 
 function fetchData () {
   const dataContainer = document.querySelector('#artist-data')
+  const artistInfo = `
+  <h1>${data.strArtist}</h1>
+  <img src= "${data.strArtistClearart}" alt="Artist pic" class = "Artist-pic"/>
+  <h2>${data.strGenre}</h2>
+  <p>${data.strWebsite}</p>
+  <p>${data.strTwitter}</p>
+  <p>${data.strFacebook}</p>   
+   `
+  console.log(artistInfo)
+  dataContainer.insertAdjacentHTML('beforeend', artistInfo)
 } 
 
+// 4.Append to Country Data
+
+const form = document.querySelector('form')
+
+// Add event handler to form 
