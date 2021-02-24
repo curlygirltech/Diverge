32a1352cb04419f16b350d5ec142f8ce7788c858
jcameron10 <jovonnec@gmail.com>
2021-02-24 11:44:20 -0500

    Added remove function

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ async function getMusic(artist) {
   const url = `http://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`
   try {
     let response = await axios.get(url)
-    let data = response.data.artists
+    let data = response.data.artists[0]
     console.log(data)
+    fetchData(data)
+    return response
   } catch (error) {
     console.error(error) 
   }
   
   }
-getMusic('drake')
+// getMusic('drake')
 
-function fetchData () {
+function fetchData (data) {
   const dataContainer = document.querySelector('#artist-data')
   const artistInfo = `
   <h1>${data.strArtist}</h1>
@@ -28,8 +30,26 @@ function fetchData () {
   dataContainer.insertAdjacentHTML('beforeend', artistInfo)
 } 
 
-// 4.Append to Country Data
+// 4.Append to Artist info
 
 const form = document.querySelector('form')
 
 // Add event handler to form 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const searchValue = document.querySelector('#search-bar').value
+  clearArtist()
+  getMusic(searchValue)
+
+  document.querySelector('#search-bar').value = ""
+})
+
+// Make a function to remove Previous Data and clear search bar
+function clearArtist() {
+  const artistContainer = document.querySelector('#artist-data')
+
+  while (artistContainer.lastChild) {
+    artistContainer.removeChild(artistContainer.lastChild)
+  }
+    
+}
